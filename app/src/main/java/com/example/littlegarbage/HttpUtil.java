91fff3a70717d042d242a30820ca484595b754f9
lly-rachel <970758884@qq.com>
2020-05-06 16:56:26 +0800

    update

--- app/src/main/java/com/example/littlegarbage/HttpUtil.java
+++ app/src/main/java/com/example/littlegarbage/HttpUtil.java
@@ -1,58 +1,86 @@
 package com.example.littlegarbage;
 
+import android.util.Log;
+
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.FormBody;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+import static android.content.ContentValues.TAG;
+
 public class HttpUtil {
+
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+
     public static String sendOkHttpRequest(String garbage) {
 
-        OkHttpClient client = new OkHttpClient();
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .connectTimeout(400, TimeUnit.SECONDS)
+                .writeTimeout(400, TimeUnit.SECONDS)
+                .readTimeout(400, TimeUnit.SECONDS)
+                .build();
 
-        String url1= "https://aiapi.jd.com/jdai/garbageTextSearch?appkey=f08733d22c104e5dc39f97a323359da9&timestamp=";
-        long time =System.currentTimeMillis();
-        String s1 = GetMD5.md5("1a8c89772abf812630f6687255d22a3b"+time);
-        String url = url1+time+"&sign="+s1;
+        String url1 = "https://aiapi.jd.com/jdai/garbageTextSearch?appkey=f08733d22c104e5dc39f97a323359da9&timestamp=";
+        long time = System.currentTimeMillis();
+        String s1 = GetMD5.md5("1a8c89772abf812630f6687255d22a3b" + time);
+        String url = url1 + time + "&sign=" + s1;
 
-        RequestBody requestBody = new FormBody.Builder()
-                .add("cityId","310000")
+//        JSONObject json = new JSONObject();
+//
+//        try {
+//            json.put("cityId","310000");
+//            json.put("text",garbage);
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+
+//        RequestBody body = FormBody.create(JSON, String.valueOf(json));
+
+//        String json = "{ "+"cityId"+":"+310000+","+"text:"+garbage+"}";
+
+//        RequestBody body = RequestBody.create(JSON, json);
+
+        RequestBody body = new FormBody.Builder()
+                .add("cityId",String.valueOf(310000))
                 .add("text",garbage)
                 .build();
 
         Request request = new Request.Builder()
                 .url(url)
-                .addHeader("Content-Type","application/json;charset=UTF-8")
-                .post(requestBody)
+                .addHeader("Content-Type", "application/json;charset=UTF-8")
+                .post(body)
                 .build();
 
-        final String[] responseData = {null};
+        Response response = null;
+        try {
+            response = okHttpClient.newCall(request).execute();
+            if (response.code()==200) {
+                String message=response.body().string();
 
-        client.newCall(request).enqueue(new Callback() {
+                return message;
+            } else {
 
-            @Override
-            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                if(response.code()==200){
-                    responseData[0] = response.body().string();
-                }
-
+                throw new IOException("Unexpected code " + response);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-            @Override
-            public void onFailure(@NotNull Call call, @NotNull IOException e) {
-
-            }
-        });
-
-
-
-        return responseData[0];
+        return null;
     }
+
+
 }
